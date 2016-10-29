c945a5c0e0b7d3b82388a953466aa240569cc7fb
Daniel Zlotin <danielzl@wix.com>
2016-10-29 21:23:26 +0300

    fixing test env

--- test/testutils.js
+++ test/testutils.js
@@ -1,8 +1,17 @@
+const appRootPath = require('app-root-path');
 const proxyquireLib = require('proxyquire');
+
+export function requireAbsolute(path, noPreserveCache = false) {
+  if (noPreserveCache) {
+    return proxyquire(path, {});
+  } else {
+    return appRootPath.require(path);
+  }
+}
 
 export function proxyquire(path, imports) {
   try {
-    return proxyquireLib.noCallThru().noPreserveCache()(path, imports);
+    return proxyquireLib.noCallThru().noPreserveCache()(appRootPath.resolve(path), imports);
   } catch (e) {
     throw new Error(`while requiring file ${path}: ${e.stack}`);
   }
