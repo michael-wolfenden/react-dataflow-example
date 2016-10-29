e6ee83b30695091adcdb2943f1b6b918ae6ce10a
Daniel Zlotin <danielzl@wix.com>
2016-10-30 00:32:12 +0300

    linted

--- src/stores/topics/actions.test.js
+++ src/stores/topics/actions.test.js
@@ -16,7 +16,6 @@ describe('store/topics/actions', () => {
   });
 
   describe('fetchTopics', () => {
-
     it('gets default subreddits and saves in the store', async() => {
       expect(mockRedditService.getDefaultSubreddits).not.toHaveBeenCalled();
 
